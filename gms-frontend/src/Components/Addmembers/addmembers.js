ce699751dd9f6eb2a7eb3b3cb8def8c04125f945
RIZKY ESA FIRMANSYAH <esafirmansyah732@gmail.com>
2025-07-06 16:20:06 +0700

    front-end finish

--- gms-frontend/src/Components/Addmembers/addmembers.js
+++ gms-frontend/src/Components/Addmembers/addmembers.js
@@ -1,20 +1,50 @@
 import React, { useState } from "react";
-
+import axios from "axios";
+import Stack from "@mui/material/Stack";
+import LinearProgress from "@mui/material/LinearProgress";
 const Addmembers = () => {
   const [inputField, setInputField] = useState({
     name: "",
     mobileName: "",
     address: "",
     membership: "",
-    profilePic: "",
+    profilePic:
+      "https://th.bing.com/th/id/OIP.gj6t3grz5no6UZ03uIluiwHaHa?rs=1&pid=ImgDetMain",
     joiningDate: "",
   });
+
+  const [imageLoader, setImageLoader] = useState(false);
 
   const handleOnChange = (event, name) => {
     setInputField({ ...inputField, [name]: event.target.value });
   };
 
   console.log(inputField);
+  const uploadImage = async (event) => {
+    setImageLoader(true);
+    console.log("Image Uploading");
+    const files = event.target.files;
+    const data = new FormData();
+    data.append("file", files[0]);
+
+    // dvpkjcisn
+
+    data.append("upload_preset", "gym-management");
+
+    try {
+      const response = await axios.post(
+        "https://api.cloudinary.com/v1_1/dvpkjcisn/image/upload",
+        data
+      );
+      console.log(response);
+      const imageUrl = response.data.url;
+      setInputField({ ...inputField, ["profilePic"]: imageUrl });
+      setImageLoader(false);
+    } catch (err) {
+      console.log(err);
+      setImageLoader(false);
+    }
+  };
 
   return (
     <div className="text-black">
@@ -60,15 +90,18 @@ const Addmembers = () => {
           <option>2 Mounth Membership</option>
         </select>
 
-        <input type="file" />
+        <input type="file" onChange={(e) => uploadImage(e)} />
 
-        <div className="w-1/4">
+        <div className="w-[100px] h-[100px]">
           <img
-            src={
-              "https://th.bing.com/th/id/OIP.gj6t3grz5no6UZ03uIluiwHaHa?rs=1&pid=ImgDetMain"
-            }
+            src={inputField.profilePic}
             className="w-full h-full rounded-full"
-          />
+          />{" "}
+          {imageLoader && (
+            <Stack sx={{ width: "100%", color: "grey.500" }} spacing={2}>
+              <LinearProgress color="secondary" />
+            </Stack>
+          )}
         </div>
 
         <div className="p-3 border-2 w-28 text-lg h-14 text-center bg-slate-900 text-white rounded-xl cursor-pointer hover:bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
--- gms-frontend/src/Components/Signup/signUp.js
+++ gms-frontend/src/Components/Signup/signUp.js
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import "./signUp.css";
 import Modal from "../Modal/modal";
 import ForgotPassword from "../ForgotPassword/forgotPassword";
+import axios from "axios";
+
+import Stack from "@mui/material/Stack";
+import LinearProgress from "@mui/material/LinearProgress";
 const SignUp = () => {
   const [forgotPassword, setForgotPassword] = useState(false);
   const [inputField, setInputField] = useState({
@@ -12,6 +16,7 @@ const SignUp = () => {
     profilePic:
       "https://images.unsplash.com/photo-1507398941214-572c25f4b1dc?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   });
+  const [loaderImage, setLoaderImage] = useState(false);
 
   const handleClose = () => {
     setForgotPassword((prev) => !prev);
@@ -21,6 +26,33 @@ const SignUp = () => {
     setInputField({ ...inputField, [name]: event.target.value });
   };
   console.log(inputField);
+
+  const uploadImage = async (event) => {
+    setLoaderImage(true);
+    console.log("Image Uploading");
+    const files = event.target.files;
+    const data = new FormData();
+    data.append("file", files[0]);
+
+    // dvpkjcisn
+
+    data.append("upload_preset", "gym-management");
+
+    try {
+      const response = await axios.post(
+        "https://api.cloudinary.com/v1_1/dvpkjcisn/image/upload",
+        data
+      );
+      console.log(response);
+      const imageUrl = response.data.url;
+      setLoaderImage(false);
+
+      setInputField({ ...inputField, ["profilePic"]: imageUrl });
+    } catch (err) {
+      console.log(err);
+      setLoaderImage(false);
+    }
+  };
 
   return (
     <div className="customSignup w-1/3 p-10 mt-20 ml-20 bg-gray-50 bg-opacity-50 h-[450px] overflow-y-auto">
@@ -63,8 +95,23 @@ const SignUp = () => {
         className="w-full mb-10 p-2 rounded-lg"
         placeholder="Enter userPassword"
       />
-      <input type="file" className="w-full mb-10 p-2 rounded-lg" />
+
+      <input
+        type="file"
+        onChange={(e) => {
+          uploadImage(e);
+        }}
+        className="w-full mb-10 p-2 rounded-lg"
+      />
+
+      {loaderImage && (
+        <Stack sx={{ width: "100%", color: "grey.500" }} spacing={2}>
+          <LinearProgress color="secondary" />
+        </Stack>
+      )}
+
       <img src={inputField.profilePic} className="mb-10 h-[200px] w-[250px]" />
+
       <div className="p-2 w-[80%] border-2 bg-slate-800 mx-auto rounded-lg text-white text-center text-lg hover:bg-white hover:text-black font-semibold cursor-pointer">
         Register
       </div>
